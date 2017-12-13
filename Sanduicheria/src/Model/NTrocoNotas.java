6f1b617f8efc7fe2fba38ef7d251ca71ffcabe5f
nicoolasaraujo <nogueira-nicolas@hotmail.com>
2017-12-13 18:21:33 -0200

    Notas

--- Sanduicheria/src/Model/NTrocoNotas.java
+++ Sanduicheria/src/Model/NTrocoNotas.java
@@ -5,6 +5,7 @@
  */
 package Model;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 /**
@@ -20,22 +21,34 @@ public class NTrocoNotas extends TrocoNotas {
 
     @Override
     public void notasTroco(double troco, ArrayList<String> s) {
+        DecimalFormat dm = new DecimalFormat("#0.00");
+        DecimalFormat dn = new DecimalFormat("#0");
 //        Long qtdNotas = Math.round(troco / this.getNotaMoeda());
         int qtdNotas = (int) (troco / this.getNotaMoeda());
 //        System.out.println(troco / this.getNotaMoeda());
         double resto = troco % this.getNotaMoeda();
 
-        if (resto != 0 && this.getNext() != null) {
+        if (resto != 0.0d && this.getNext() != null && resto >= 0.05d) {
 //            this.getNotas().add(qtdNotas + " de" + getNotaMoeda());
             if (qtdNotas != 0) {
-                s.add(qtdNotas + " de " + getNotaMoeda());
+                if (this.getNotaMoeda() > 1.0d) {
+                    s.add(qtdNotas + " nota(s) de " + dn.format(getNotaMoeda()));
+                } else {
+                    s.add(qtdNotas + " moeda(s) de " + dm.format(getNotaMoeda()));
+                }
             }
 //            System.out.println(qtdNotas + " de" + getNotaMoeda());
 //            System.out.println("Resto: " + resto);
             this.getNext().notasTroco(resto, s);
         } else {
 //            this.getNotas().add(qtdNotas + " de " + getNotaMoeda() + "\n");
-            s.add(qtdNotas + " de " + getNotaMoeda());
+
+            if (this.getNotaMoeda() > 1.0d) {
+                s.add(qtdNotas + " nota(s) de " + dn.format(getNotaMoeda()));
+            } else {
+                s.add(qtdNotas + " moeda(s) de " + dm.format(getNotaMoeda()));
+            }
+
 //            System.out.println(qtdNotas + " de " + getNotaMoeda());
 //            System.out.println("Resto: " + resto);
         }
